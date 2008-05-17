df05365f76424ac35f5f7c6d9b9e5701f2de219b
martin <martin@laptop.DUQUESNOY>
2008-05-17 13:24:06 +0200

    tty-snake config file

---
+++ config.h
@@ -0,0 +1,30 @@
+/* COLOR */
+
+/* border color */
+#define BORDER_COLOR white
+#define FGBORDER_COLOR white
+/* info color */
+#define INFO_COLOR white
+#define FGINFO_COLOR black
+/* snake color */
+#define SNAKE_COLOR green
+#define FGSNAKE_COLOR black
+/* food color */
+#define FOOD_COLOR blue
+#define FGFOOD_COLOR black
+/* random-wall color */
+#define RANDW_COLOR red
+#define FGRANDW_COLOR black
+
+/* OPTION */
+
+/* speed */
+#define O_SPEED 600
+/* random wall */
+#define O_RANDW  FALSE
+/* random level */
+#define O_RANDL FALSE
+/* bell option */
+#define O_BELL FALSE
+
+
